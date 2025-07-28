891621b795c9b866923bb1235786a0662e8ed473
SMTK <smtktom@gmail.com>
2025-07-28 15:03:27 +0200

    refreshing prints, vars, data types, nums, strings, working with text

---
+++ python-basics/basics.py
@@ -0,0 +1,48 @@
+# printing
+print("Hello, World!")
+
+# variables
+name = "Tomáš"
+age = 30
+favorite_food = "kebab"
+print(f"My name is {name} and I am {age} years old and my favorite food is {favorite_food}.")
+
+# data types
+integer_number = 42  # celé číslo
+float_number = 3.14  # desetinné číslo
+
+# basic operations with numbers
+a = 10
+b = 3
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+
+exc = 15 * 7
+print("Výsledek je", exc)
+
+# strings
+pozdrav = "Ahoj"
+zprava = "Jak se máš?"
+dlouhy_text = """Toto je
+víceřádkový
+text"""
+
+# work with text
+name = "Tom"
+print(len(name))
+print(name.upper())
+print(name.lower())
+print("om" in name)  # True
+
+# concentation text
+first_name = "Tomáš"
+last_name = "Smutek"
+full_name = first_name + " " + last_name
+print(full_name)
+print(f"{first_name} {last_name}")
+
