1155ba796ca30757c278bcef0eb05b47b8566472
SMTK <smtktom@gmail.com>
2025-09-13 13:43:44 +0200

    refresh of some buil-in functions

---
+++ python-basics/builtin_funcs.py
@@ -0,0 +1,10 @@
+# Vestavěné funkce v pythonu, které máme již k dispozici k použití
+# Je jich mnohem více k různému použití, zde pouze pár
+
+numbers = [1, 2, 3, 4, 5]  # list čísel
+
+print(len(numbers))  # len() vypočítá počet položek v seznamu
+print(sum(numbers))  # sum() sečtě všechny čísla
+print(max(numbers))  # max() najde největší číslo
+print(min(numbers))  # min() najde nejmenší číslo
+print(type(numbers)) # type() ti poví o jaký datový typ objektu jde
