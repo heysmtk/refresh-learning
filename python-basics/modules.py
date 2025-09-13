7a75fff901e291a582ffa89327be8af68b156783
SMTK <smtktom@gmail.com>
2025-09-13 14:41:30 +0200

    refresh of modules

---
+++ python-basics/modules.py
@@ -0,0 +1,6 @@
+# Moduly = další python soubory s různými funkcemi, proměnnýmí, které lze importovat
+# Jde o jakýsi pořádek v kódu
+# Moduly jsou vestavěné jako math, random a nebo naše vlastní
+
+import my_module
+my_module.pozdrav()  # "Ahoj z modulu!"
---
+++ python-basics/my_module.py
@@ -0,0 +1,4 @@
+# Můj modul, který se ale po importu provádí v modules.py
+
+def pozdrav():
+    print("Ahoj z modulu!")
