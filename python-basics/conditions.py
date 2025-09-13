d6e348131b88ff106b4d094cb1fb4e069577247c
SMTK <smtktom@gmail.com>
2025-09-13 13:01:06 +0200

    refresh of tuples

--- python-basics/conditions.py
+++ python-basics/conditions.py
@@ -1,8 +1,8 @@
-age = 30
+age = 30  # proměnná
 
-if age >= 18:
+if age >= 18:  # když je age větší nebo rovno 18
     print("You're adult!")
-elif age >= 12:
+elif age >= 12: # když je age větší nebo rovno 12
     print("You're teenager")
-else:
+else:  # když není platná žádná z výše uvedených podmínek
     print("Child")
---
+++ python-basics/tuples.py
@@ -0,0 +1,6 @@
+# Tuples taky jako N-tice
+# Jsou podobné jako seznamy, ale neměnitelné (immutable)
+# Hodí se a používá se pro data, která by se neměly nikdy měnit!
+
+colors = ("red", "green", "blue")
+print(colors[0])  # red
