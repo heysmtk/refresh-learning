0af14432a7e7e84d1af6e7240f92568e20354421
SMTK <smtktom@gmail.com>
2025-09-14 13:59:18 +0200

    move soubor.txt file, edit filepath

--- python-basics/fileswork.py
+++ python-basics/fileswork.py
@@ -5,26 +5,26 @@
 # with - automatické zavře soubor = with open()
 
 # Zápis do souboru
-with open("soubor.txt", "w") as f:
+with open("python-basics/soubor.txt", "w") as f:
     f.write("Ahoj soubore, jsem tady!\n")
  
 # přidání dalšího textu do souboru   
-with open("soubor.txt", "a") as f:
+with open("python-basics/soubor.txt", "a") as f:
     f.write("Tohle je další řádek!\n")
     
 # čtení ze souboru
-with open("soubor.txt","r") as f:
+with open("python-basics/soubor.txt","r") as f:
     print(f.read())
 
 # vyčte řádek po řádku, strip() odstraní prázné řádky a mezery
-with open("soubor.txt", "r") as f:
+with open("python-basics/soubor.txt", "r") as f:
     for line in f:
         print("line:", line.strip())
 
 # readline vyčte jeden řádek ze souboru
-with open("soubor.txt", "r") as f:
+with open("python-basics/soubor.txt", "r") as f:
     print(f.readline())
     
 # readlines vyčte seznam všech řádků
-with open("soubor.txt", "r") as f:
-print(f.readlines())
+with open("python-basics/soubor.txt", "r") as f:
+    print(f.readlines())
