05d7c6872f25e3960ddd0959edd4133a564cd1fc
SMTK <smtktom@gmail.com>
2025-09-17 21:48:35 +0200

    refresh of classes and objects

---
+++ python-oop/class_object.py
@@ -0,0 +1,11 @@
+# class = je to šablona (plán, blueprint), podle které se dělají objekty
+# object = konkrétní instance (výsledek) té oné šablony
+
+# Proč to tak je?
+# Bez tříd by musel být vše napsané "procedurálně", OOP mi
+# umožnuje spojit data a funkce, které spolu souvisí do jednoho objektu!
+
+class Dog:  # definice šablony
+    pass
+
+my_dog = Dog()  # vytvoření objektu z té šablony
